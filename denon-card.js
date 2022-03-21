ddf6b6921a41cab1bb3b3659d643fcddb14f845e
John D. Martin III <me@johndmart.in>
2022-03-21 00:13:25 -0400

    add 4 button rows

--- denon-card.js
+++ denon-card.js
@@ -4,6 +4,7 @@ const LitElement = Object.getPrototypeOf(
 
 const html = LitElement.prototype.html;
 
+/*
 import {
 mdiPower,
 mdiPowerOn,
@@ -27,7 +28,7 @@ mdiVolumeMinus,
 mdiVolumePlus,
 mdiNetflix,
 mdiYoutube,
-} from "https://unpkg.com/@mdi/js@6.4.95/mdi.js?module"
+} from "https://unpkg.com/@mdi/js@6.4.95/mdi.js?module" */
 
 // Taken from mdi v5.9.55
 const AMAZON_ICON_PATH = "M15.93,17.09C15.75,17.25 15.5,17.26 15.3,17.15C14.41,16.41 14.25,16.07 13.76,15.36C12.29,16.86 11.25,17.31 9.34,17.31C7.09,17.31 5.33,15.92 5.33,13.14C5.33,10.96 6.5,9.5 8.19,8.76C9.65,8.12 11.68,8 13.23,7.83V7.5C13.23,6.84 13.28,6.09 12.9,5.54C12.58,5.05 11.95,4.84 11.4,4.84C10.38,4.84 9.47,5.37 9.25,6.45C9.2,6.69 9,6.93 8.78,6.94L6.18,6.66C5.96,6.61 5.72,6.44 5.78,6.1C6.38,2.95 9.23,2 11.78,2C13.08,2 14.78,2.35 15.81,3.33C17.11,4.55 17,6.18 17,7.95V12.12C17,13.37 17.5,13.93 18,14.6C18.17,14.85 18.21,15.14 18,15.31L15.94,17.09H15.93M13.23,10.56V10C11.29,10 9.24,10.39 9.24,12.67C9.24,13.83 9.85,14.62 10.87,14.62C11.63,14.62 12.3,14.15 12.73,13.4C13.25,12.47 13.23,11.6 13.23,10.56M20.16,19.54C18,21.14 14.82,22 12.1,22C8.29,22 4.85,20.59 2.25,18.24C2.05,18.06 2.23,17.81 2.5,17.95C5.28,19.58 8.75,20.56 12.33,20.56C14.74,20.56 17.4,20.06 19.84,19.03C20.21,18.87 20.5,19.27 20.16,19.54M21.07,18.5C20.79,18.14 19.22,18.33 18.5,18.42C18.31,18.44 18.28,18.26 18.47,18.12C19.71,17.24 21.76,17.5 22,17.79C22.24,18.09 21.93,20.14 20.76,21.11C20.58,21.27 20.41,21.18 20.5,21C20.76,20.33 21.35,18.86 21.07,18.5Z";
@@ -555,6 +556,106 @@ class DenonCardServices extends LitElement {
                 `
               : ""
           }
+          
+
+          ${
+            this._config.sound_mode_left || this._config.sound_mode_right || this._config.audio_delay_decrease || this._config.audio_delay_increase
+              ? html`
+                  <div class="row">
+                    ${this._config.quick1
+                      ? html`
+                          <ha-icon-button
+                            .action="${"quick1"}"
+                            @click="${this.handleActionClick}"
+                            title="Quick Select 1"
+                          ><ha-icon icon="mdi:numeric-1-circle"></ha-icon
+                          ></ha-icon-button>
+                        `
+                      : emptyButton}
+                    ${this._config.quick2
+                      ? html`
+                          <ha-icon-button
+                            .action="${"quick2"}"
+                            @click="${this.handleActionClick}"
+                            title="Quick Select 2"
+                          ><ha-icon icon="mdi:numeric-2-circle"></ha-icon
+                          ></ha-icon-button>
+                        `
+                      : emptyButton}
+                    ${this._config.quick3
+                      ? html`
+                          <ha-icon-button
+                            .action="${"quick3"}"
+                            @click="${this.handleActionClick}"
+                            title="Quick Select 3"
+                          ><ha-icon icon="mdi:numeric-3-circle"></ha-icon
+                          ></ha-icon-button>
+                        `
+                      : emptyButton}
+                    ${this._config.quick4
+                      ? html`
+                          <ha-icon-button
+                            .action="${"quick4"}"
+                            @click="${this.handleActionClick}"
+                            title="QUick Select 4"
+                          ><ha-icon icon="mdi:numeric-4-circle"></ha-icon
+                          ></ha-icon-button>
+                        `
+                      : emptyButton}
+                  </div>
+                `
+              : ""
+          }
+          
+          ${
+            this._config.sound_mode_left || this._config.sound_mode_right || this._config.audio_delay_decrease || this._config.audio_delay_increase
+              ? html`
+                  <div class="row">
+                    ${this._config.sound_mode_left
+                      ? html`
+                          <ha-icon-button
+                            .action="${"sound_mode_left"}"
+                            @click="${this.handleActionClick}"
+                            title="Sound Mode Left"
+                          ><ha-icon icon="mdi:chevron-left"></ha-icon
+                          ></ha-icon-button>
+                        `
+                      : emptyButton}
+                    ${this._config.sound_mode_right
+                      ? html`
+                          <ha-icon-button
+                            .action="${"sound_mode_right"}"
+                            @click="${this.handleActionClick}"
+                            title="Sound Mode Right"
+                          ><ha-icon icon="mdi:chevron-right"></ha-icon
+                          ></ha-icon-button>
+                        `
+                      : emptyButton}
+                    ${this._config.audio_delay_decrease
+                      ? html`
+                          <ha-icon-button
+                            .action="${"audio_delay_decrease"}"
+                            @click="${this.handleActionClick}"
+                            title="Audio Delay -"
+                          ><ha-icon icon="mdi:minus"></ha-icon
+                          ></ha-icon-button>
+                        `
+                      : emptyButton}
+                    ${this._config.audio_delay_increase
+                      ? html`
+                          <ha-icon-button
+                            .action="${"audio_delay_increase"}"
+                            @click="${this.handleActionClick}"
+                            title="Audio Delay +"
+                          ><ha-icon icon="mdi:minus"></ha-icon
+                          ></ha-icon-button>
+                        `
+                      : emptyButton}
+                  </div>
+                `
+              : ""
+          }
+          
       </ha-card>
     `;
   }
@@ -641,7 +742,15 @@ class DenonCardServices extends LitElement {
       "forward",
       "channel_level",
       "mode",
-      "memory"
+      "memory",
+      "quick1",
+      "quick2",
+      "quick3",
+      "quick4",
+      "sound_mode_left",
+      "sound_mode_right",
+      "audio_delay_decrease",
+      "audio_delay_increase"
     ];
 
     if (
